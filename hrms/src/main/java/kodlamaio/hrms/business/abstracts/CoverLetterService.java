0f813bba13d92c3c56faa76b71d08eafc492b3c9
Furkan394 <furkanncakiroglu@gmail.com>
2021-06-02 14:37:31 +0300

    cover letters layers added

---
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/CoverLetterService.java
@@ -0,0 +1,13 @@
+package kodlamaio.hrms.business.abstracts;
+
+import java.util.List;
+
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.entities.concretes.CoverLetter;
+
+public interface CoverLetterService {
+
+	DataResult<List<CoverLetter>> getAll();
+	Result add(CoverLetter coverLetter);
+}
---
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/CoverLetterManager.java
@@ -0,0 +1,39 @@
+package kodlamaio.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.hrms.business.abstracts.CoverLetterService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
+import kodlamaio.hrms.dataAccess.abstracts.CoverLetterDao;
+import kodlamaio.hrms.entities.concretes.CoverLetter;
+
+@Service
+public class CoverLetterManager implements CoverLetterService{
+	
+	private CoverLetterDao coverLetterDao;
+
+	@Autowired
+	public CoverLetterManager(CoverLetterDao coverLetterDao) {
+		super();
+		this.coverLetterDao = coverLetterDao;
+	}
+
+	@Override
+	public DataResult<List<CoverLetter>> getAll() {
+		
+		return new SuccessDataResult<List<CoverLetter>>(this.coverLetterDao.findAll(),"Cover letters listed.");
+	}
+
+	@Override
+	public Result add(CoverLetter coverLetter) {
+		this.coverLetterDao.save(coverLetter);
+		return new SuccessResult("Cover letter added");
+	}
+
+}
