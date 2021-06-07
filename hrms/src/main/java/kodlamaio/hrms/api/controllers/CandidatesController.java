7f4ad6e334e2cd0a91f400e17e223ea9af9ae810
Furkan394 <furkanncakiroglu@gmail.com>
2021-06-07 19:25:28 +0300

    cross origin anotation added

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/CandidatesController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/CandidatesController.java
@@ -3,6 +3,7 @@ package kodlamaio.hrms.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import kodlamaio.hrms.entities.dtos.CvDto;
 
 @RestController
 @RequestMapping("/api/candidates")
+@CrossOrigin
 public class CandidatesController {
 
 	private CandidateService candidateService;
